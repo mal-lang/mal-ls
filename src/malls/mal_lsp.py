5e086a278cce300f5994226f6231e00c02d8a821
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-05-22 17:07:26 +0200

    feat: Language server base inheritance

--- src/malls/mal_lsp.py
+++ src/malls/mal_lsp.py
@@ -1,5 +1,6 @@
 import logging
 import typing
+from pylsp_jsonrpc.dispatchers import MethodDispatcher
 
 log = logging.getLogger(__name__)
 MAL_FILETYPES = (".mal",)
@@ -7,3 +8,6 @@ MAL_FILETYPES = (".mal",)
 
 def start_fileio_server(in_file: typing.BinaryIO, out_file: typing.BinaryIO) -> None:
     log.info("Starting MAL LSP IO language server.")
+
+class MALLSPServer(MethodDispatcher):
+    pass
