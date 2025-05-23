38943dc69f08c4df88da6bb27c1ea272e38b786b
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-05-23 11:10:32 +0200

    feat: define init for MALLSPServer

--- src/malls/mal_lsp.py
+++ src/malls/mal_lsp.py
@@ -2,6 +2,8 @@ import logging
 import typing
 
 from pylsp_jsonrpc.dispatchers import MethodDispatcher
+from pylsp_jsonrpc.endpoint import Endpoint
+from pylsp_jsonrpc.streams import JsonRpcStreamReader, JsonRpcStreamWriter
 
 log = logging.getLogger(__name__)
 MAL_FILETYPES = (".mal",)
@@ -12,4 +14,11 @@ def start_fileio_server(in_file: typing.BinaryIO, out_file: typing.BinaryIO) -> 
 
 
 class MALLSPServer(MethodDispatcher):
-    pass
+    def __init__(
+            self,
+            input: typing.BinaryIO | None = None,
+            output: typing.BinaryIO | None = None) -> None:
+        self.__jsonrpc_stream_reader = JsonRpcStreamReader(input) if input else None
+        self.__jsonrpc_stream_writer = JsonRpcStreamWriter(output) if output else None
+
+        self.__endpoint = Endpoint(self, self.__jsonrpc_stream_writer.write)
