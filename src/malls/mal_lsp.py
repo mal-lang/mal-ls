9e13a8180e3bb9eea467f1ab8f54ef2810e04979
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-05-23 15:43:10 +0200

    feat: shutdown and exit methods, and relevant notification
    Implements the shutdown and exit methods of LSP and the server -> client
    notification invalid_request_after_shutdown if there is a request
    between shutdown and exit.

--- src/malls/mal_lsp.py
+++ src/malls/mal_lsp.py
@@ -5,6 +5,8 @@ from pylsp_jsonrpc.dispatchers import MethodDispatcher
 from pylsp_jsonrpc.endpoint import Endpoint
 from pylsp_jsonrpc.streams import JsonRpcStreamReader, JsonRpcStreamWriter
 
+from .lsp.enums import ErrorCodes
+
 log = logging.getLogger(__name__)
 MAL_FILETYPES = (".mal",)
 
@@ -22,7 +24,33 @@ class MALLSPServer(MethodDispatcher):
 
         self.__endpoint = Endpoint(self, self.__jsonrpc_stream_writer.write)
 
+        self.__shutdown = False
+
     def start(self) -> None:
         """Starts the language server."""
         log.info("Starting MAL LSP language server.")
         self.__jsonrpc_stream_reader.listen(self.__endpoint.consume)
+
+    def m_shutdown(self, **kwargs) -> None:
+        log.info("Received shutdown request.")
+        self.__shutdown = True
+
+    def m_invalid_request_after_shutdown(self, **kwargs):
+        log.warn("Received request after shutdown.")
+        return {
+            "error": {
+                "code": ErrorCodes.InvalidRequest,
+                "message": "Requests after `shutdown` are not valid.",
+            }
+        }
+
+    def m_exit(self, **kwargs) -> None:
+        log.info("Exiting language server.")
+        self._endpoint.shutdown()
+        log.info("Endpoint shut down.")
+        if self._jsonrpc_stream_reader:
+            self._jsonrpc_stream_reader.close()
+            log.info("JSON RPC reader closed.")
+        if self._jsonrpc_stream_writer:
+            self._jsonrpc_stream_writer.close()
+            log.info("JSON RPC writer closed.")
