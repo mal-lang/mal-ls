f1b982db62867c9afb843a884b490e950ee6d6d9
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-06-10 14:05:52 +0200

    test: Shutdown right after initalize errs

--- tests/integration/test_base_protocol.py
+++ tests/integration/test_base_protocol.py
@@ -3,13 +3,16 @@ import io
 import logging
 import typing
 
-from malls.mal_lsp import MALLSPServer
+from malls.lsp.enums import ErrorCodes
 from malls.lsp.fsm import LifecycleState
+from malls.mal_lsp import MALLSPServer
+
+from ..util import get_lsp_json
 
 log = logging.getLogger(__name__)
 
 # wait for most 5s (arbitrary)
-MAX_TIMEOUT=5
+MAX_TIMEOUT=2
 
 class SteppedBytesIO(io.BytesIO):
     """
@@ -24,34 +27,62 @@ class SteppedBytesIO(io.BytesIO):
         else:
             self.steps -= 1
 
-async def server_output(
+# TODO: create fake endpoint class (or similar) so the input can be stepped
+
+# https://github.com/python-lsp/python-lsp-server/blob/develop/pylsp/python_lsp.py#L58
+def server_output(
         input: typing.BinaryIO,
-        timeout: float | None = MAX_TIMEOUT) -> typing.Tuple[typing.BinaryIO, MALLSPServer]:
+        timeout: float | None = MAX_TIMEOUT) -> typing.Tuple[typing.BinaryIO, MALLSPServer, TimeoutError | None]:
     intermediary = SteppedBytesIO()
     ls = MALLSPServer(input, intermediary)
+    time_out_err = None
 
     async def run_server():
         ls.start()
+
     try:
-        await asyncio.wait_for(run_server(), timeout=timeout)
+        server_future = run_server()
+        asyncio.run(asyncio.wait_for(server_future, 1))
+    except TimeoutError as e:
+        ls.m_exit()
+        time_out_err = e
     except Exception as e:
         intermediary.close()
         raise e
 
-    return intermediary, ls
+    return intermediary, ls, time_out_err
 
 
-async def test_correct_base_lifecycle(
+def test_correct_base_lifecycle(
         init_exit_in: typing.BinaryIO,
         init_exit_out: typing.BinaryIO):
-    output, _ = await server_output(init_exit_in)
+    output, *_ = server_output(init_exit_in)
 
     assert output.getvalue() == init_exit_out.read()
     output.close()
 
-async def test_pre_initialized_exit_does_not_change_state(
+def test_pre_initialized_exit_does_not_change_state(
         pre_initialized_exit_in: typing.BinaryIO):
-    output, ls = await server_output(pre_initialized_exit_in)
+    output, ls, *_ = server_output(pre_initialized_exit_in)
 
     assert ls.state.current_state == LifecycleState.INITIALIZE
+    output.close()
+
+def test_pre_initialized_shutdown_does_not_change_state(
+        pre_initialized_shutdown_in: typing.BinaryIO):
+    output, ls, *_ = server_output(pre_initialized_shutdown_in)
+
+    assert ls.state.current_state == LifecycleState.INITIALIZE
+    output.close()
+
+def test_pre_initialized_shutdown_errs(
+        pre_initialized_shutdown_in: typing.BinaryIO):
+    output, ls, *_ = server_output(pre_initialized_shutdown_in)
+
+    response = get_lsp_json(output)
+    assert "capabilities" in response.result
+
+    response = get_lsp_json(output)
+    assert response.error.code == ErrorCodes.InvalidRequest
+
     output.close()
