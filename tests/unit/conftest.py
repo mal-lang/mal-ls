b4bd01aad79660d060c7a9cdc23400f1a4fe9a14
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-06-17 15:03:08 +0200

    misc: Fake language server fixture for unit tests

---
+++ tests/unit/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from io import BytesIO
+
+from ..util import FakeLanguageServer
+from malls.lsp.fsm import LifecycleState
+
+@pytest.fixture
+def mute_ls() -> FakeLanguageServer:
+    ls = FakeLanguageServer(BytesIO, BytesIO)
+    yield ls
+    if ls.state.current_state != LifecycleState.EXIT:
+        ls.m_exit()
