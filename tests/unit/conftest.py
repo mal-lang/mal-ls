82c86645c6dd877384c0d8e3317f31d760477d86
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-06-18 17:27:10 +0200

    fix: instance bytesio classes, not just passing types/classes

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -6,7 +6,7 @@ from malls.lsp.fsm import LifecycleState
 
 @pytest.fixture
 def mute_ls() -> FakeLanguageServer:
-    ls = FakeLanguageServer(BytesIO, BytesIO)
+    ls = FakeLanguageServer(BytesIO(), BytesIO())
     yield ls
     if ls.state.current_state != LifecycleState.EXIT:
         ls.m_exit()
