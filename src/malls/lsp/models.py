d1bc2b24e7bf18f2553ee988f03ce3e53337ef5d
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-06-24 15:37:06 +0200

    feat: Add validating ProgressParams model

--- src/malls/lsp/models.py
+++ src/malls/lsp/models.py
@@ -11,3 +11,19 @@ class CancelParams(BaseModel, TypedDict):
 
     id: int | str
     """The request id to cancel."""
+
+class ProgressParams[T](BaseModel, TypedDict):
+    """
+    The base protocol offers also support to report progress in a generic fashion. This mechanism
+    can be used to report any kind of progress including work done progress (usually used to report
+    progress in the user interface using a progress bar) and partial result progress to support
+    streaming of results.
+
+    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#progress
+    """
+
+    token: int | str
+    """The progress token provided by the client or server."""
+
+    value: T
+    """The progress data."""
