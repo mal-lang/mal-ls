8af833c32739e9c66c93c84b152e2dfd005de8b6
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-06-25 14:28:51 +0200

    feat: Add insert cursor position model

--- src/malls/lsp/models.py
+++ src/malls/lsp/models.py
@@ -65,3 +65,24 @@ class RegularExpressionsClientCapabilities(BaseModel, TypedDict):
 
     version: str | None
     """The engine's version."""
+
+class Position(BaseModel, TypedDict):
+    """
+    Position in a text document expressed as zero-based line and zero-based character offset. A
+    position is between two characters like an ‘insert’ cursor in an editor. Special values like
+    for example -1 to denote the end of a line are not supported.
+
+    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#position
+    """
+
+    line: UInteger
+    """Line position in a document (zero-based)."""
+
+    character: UInteger
+    """
+    Character offset on a line in a document (zero-based). The meaning of this
+	offset is determined by the negotiated `PositionEncodingKind`.
+
+	If the character value is greater than the line length it defaults back
+	to the line length.
+    """
