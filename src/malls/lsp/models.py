8b63ebdceeaf8f26c0a2d027771f4d7725a0e63f
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-06-25 15:12:20 +0200

    feat: Add document filter model

--- src/malls/lsp/models.py
+++ src/malls/lsp/models.py
@@ -187,3 +187,33 @@ class TextDocumentPositionParams(BaseModel, TypedDict):
 
     position: Position
     """The position inside the text document."""
+
+class DocumentFilter(BaseModel, TypedDict):
+    """
+    A document filter denotes a document through properties like `language`, `scheme` or `pattern`
+
+    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#documentFilter
+    """
+
+    language: str | None
+    """A language id, like `typescript`."""
+
+    scheme: str | None
+    """A Uri scheme, like `file` or `untitled`."""
+
+    pattern: str | None
+    """
+	A glob pattern, like `*.{ts,js}`.
+
+    Glob patterns can have the following syntax:
+    - `*` to match zero or more characters in a path segment
+    - `?` to match on one character in a path segment
+    - `**` to match any number of path segments, including none
+    - `{}` to group sub patterns into an OR expression. (e.g. `**/*.{ts,js}`
+        matches all TypeScript and JavaScript files)
+    - `[]` to declare a range of characters to match in a path segment
+      (e.g., `example.[0-9]` to match on `example.0`, `example.1`, …)
+    - `[!...]` to negate a range of characters to match in a path segment
+      (e.g., `example.[!0-9]` to match on `example.a`, `example.b`, but
+       not `example.0`)
+    """
