95f0c982946aec6841711a0b676f8b7748bc2060
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-06-30 14:45:57 +0200

    feat: Add command model

--- src/malls/lsp/models.py
+++ src/malls/lsp/models.py
@@ -457,3 +457,30 @@ class Diagnostic(BaseModel, TypedDict):
     """
 
     model_config = base_config
+
+class Command(BaseModel, TypedDict):
+    """
+    Represents a reference to a command. Provides a title which will be used to represent a
+    command in the UI. Commands are identified by a string identifier. The recommended way to
+    handle commands is to implement their execution on the server side if the client and server
+    provides the corresponding capabilities. Alternatively the tool extension code could handle
+    the command. The protocol currently doesn’t specify a set of well-known commands.
+
+    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#command
+    """
+
+    title: str
+    """
+    Title of the command, like `save`.
+    """
+
+    command: str
+    """
+    The identifier of the actual command handler.
+    """
+
+    arguments: list[LSPAny] | None
+    """
+    Arguments that the command handler should be
+    invoked with.
+    """
