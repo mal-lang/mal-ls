72fff0c2e793cde4f97c4302db851152cdd70c2b
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-07-08 16:23:51 +0200

    feat: Add CompletionClientCapabilities completionItemKind property model

--- src/malls/lsp/models.py
+++ src/malls/lsp/models.py
@@ -1095,3 +1095,22 @@ class CompletionItemCapabilities(BaseModel, TypedDict):
     """
 
     model_config = base_config
+
+class CompletionItemKindCapabilities(BaseModel, TypedDict):
+    """
+    The completion item kind values the client supports. When this
+    property exists the client also guarantees that it will
+    handle values outside its set gracefully and falls back
+    to a default value when unknown.
+
+    If this property is not present the client only supports
+    the completion items kinds from `Text` to `Reference` as defined in
+    the initial version of the protocol.
+
+    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#completionClientCapabilities
+    """
+
+    value_set: list[CompletionItemKind] | None
+
+    model_config = base_config
+
