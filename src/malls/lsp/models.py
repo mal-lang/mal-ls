e01f06ce1382b0347c6b692c1100c83565c3cb91
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-07-16 14:39:51 +0200

    feat: Add InlineValueWorkspaceClientCapabilities model

--- src/malls/lsp/models.py
+++ src/malls/lsp/models.py
@@ -2103,3 +2103,23 @@ class CodeLensWorkspaceClientCapabilities(BaseModel, TypedDict):
     """
 
     model_config = base_config
+
+class InlineValueWorkspaceClientCapabilities(BaseModel, TypedDict):
+    """
+    Client workspace capabilities specific to inline values.
+
+    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#inlineValueWorkspaceClientCapabilities
+    """
+
+    refresh_support: bool
+    """
+    Whether the client implementation supports a refresh request sent from
+    the server to the client.
+
+    Note that this event is global and will force the client to refresh all
+    inline values currently shown. It should be used with absolute care and
+    is useful for situation where a server for example detect a project wide
+    change that requires such a calculation.
+    """
+
+    model_config = base_config
