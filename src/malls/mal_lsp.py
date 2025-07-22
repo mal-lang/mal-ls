c01f8cdd91f471bb41c2bab6c570deeaa205cd72
Tomás Fonseca <tfonseca@kth.se>
2025-07-22 19:34:35 +0100

    fix: Corrected wrong tests and uploaded missing file.

--- src/malls/mal_lsp.py
+++ src/malls/mal_lsp.py
@@ -214,11 +214,24 @@ class MALLSPServer(MethodDispatcher):
             self.__jsonrpc_stream_writer.close()
             log.info("JSON RPC writer closed.")
 
-    def m___set_trace(self, params: dict | None) -> None:
+    '''
+    def m___set_trace(self, value) -> None:
         # For a notification, there is no response,
         # even if there is an error, so the function
         # shall just return
-        parameters = models.SetTraceParams(params) if params else None
+        parameters = models.SetTraceParams(value = value) if value else None
+        if parameters:
+            try:
+                self._change_trace_value(parameters.value)
+            finally:
+                return
+    '''
+
+    def m___set_trace(self, **kwargs: dict | None) -> None:
+        # For a notification, there is no response,
+        # even if there is an error, so the function
+        # shall just return
+        parameters = models.SetTraceParams(value = kwargs["value"]) if kwargs else None
         if parameters:
             try:
                 self._change_trace_value(parameters.value)
