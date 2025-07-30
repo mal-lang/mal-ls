45c1ad3647922b25feb8685e4a4fc772c844f936
Tomás Fonseca <tfonseca@kth.se>
2025-07-30 18:50:55 +0100

    tests: didOpen with included file.

--- tests/integration/test_did_open_text_document_notification.py
+++ tests/integration/test_did_open_text_document_notification.py
@@ -63,3 +63,49 @@ def test_open_file_without_include(writeable_fixtures_did_open_notif_in: typing.
     # parameter, since "traceValue" is wrong
 
     output.close()
+
+def test_open_file_with_include(writeable_fixtures_did_open_notif_in: typing.BinaryIO):
+    # go to start of file
+    writeable_fixtures_did_open_notif_in.seek(0)
+    # ignore first 4 lines (153 bytes)
+    writeable_fixtures_did_open_notif_in.seek(153)
+
+    # create fake uri for the MAL file being parsed 
+    # (won't be used by the server, so there is no issue if the file does not actually exist)
+    simplified_file_path = FILE_PATH + "main.mal"
+    # path for the included file
+    included_path_file = FILE_PATH + "find_current_scope_function.mal"
+    file_path = filepath_to_uri(simplified_file_path)
+
+    # construct payload with correct uri
+    payload = {"jsonrpc":"2.0","method":"textDocument/didOpen","params":{"textDocument":{"uri":file_path,"languageId":"mal","version":0,"text":"#id: \"org.mal-lang.testAnalyzer\"\n#version:\"0.0.0\"\ninclude \"find_current_scope_function.mal\"\ncategory System {\nabstract asset Foo {}\nasset Bar extends Foo {}\n}\n\n"}}}
+
+    # get the length of the payload (+1 for the newline)
+    json_string = json.dumps(payload, separators=(',', ':'))  # Remove extra spaces
+    json_payload = json_string.encode('utf-8')
+    payload_size = str(len(json_payload)+1)
+
+    # write payload and size to file
+    writeable_fixtures_did_open_notif_in.write(b"Content-Length: "+payload_size.encode())
+    writeable_fixtures_did_open_notif_in.write(b"\n\n"+json_string.encode())
+
+    # go back to start of file
+    writeable_fixtures_did_open_notif_in.seek(0)
+
+    # send to server
+    output, ls, *_ = server_output(writeable_fixtures_did_open_notif_in)
+
+    # Ensure LSP stored everything correctly
+    assert len(ls.files().keys())==2
+    assert simplified_file_path in ls.files().keys()
+    assert type(ls.files()[simplified_file_path]) == Tree
+    assert included_path_file in ls.files().keys()
+    assert type(ls.files()[included_path_file]) == Tree
+    
+    # delete added lines
+    writeable_fixtures_did_open_notif_in.seek(0)
+    writeable_fixtures_did_open_notif_in.truncate(153)
+    # Ensure there is an error on the response corresponding to invalid
+    # parameter, since "traceValue" is wrong
+
+    output.close()
