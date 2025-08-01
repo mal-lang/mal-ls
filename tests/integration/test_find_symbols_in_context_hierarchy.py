0463773d1eaf01fe7f93c7614673b99ecd2f0eaa
Tomas Fonseca <tomas.s.fonseca@tecnico.ulisboa.pt>
2025-08-01 16:44:00 +0200

    tests: Asset hierarchy symbols.

--- tests/integration/test_find_symbols_in_context_hierarchy.py
+++ tests/integration/test_find_symbols_in_context_hierarchy.py
@@ -79,3 +79,78 @@ def test_find_symbols_in_associations_hierarchy(mal_find_symbols_in_scope):
     for keyword, lvl in keywords:
         assert keyword in returned_keywords
         assert returned_keywords[keyword][1] == lvl
+
+def test_find_symbols_in_asset1_hierarchy(mal_find_symbols_in_scope):
+    tree = PARSER.parse(mal_find_symbols_in_scope.read())
+
+    # space between category and asset (category scope)
+    point = (6, 4)
+
+    # symbols (identifiers + keywords)
+    symbols = [
+        ("var",0),
+        ("c",0),
+        ("compromise",0),
+        ("destroy",0),
+        ('Asset1',1),
+        ('Asset2',1),
+        ('Asset3',1),
+    ]
+    keywords = [ 
+        ("let",0),
+        ("extends",1),
+        ("abstract",1),
+        ("asset",1),
+        ("category",2),
+        ("associations",2),
+    ]
+
+    # we use sets to ensure order does not matter
+    returned_user_symbols, returned_keywords = find_symbols_in_context_hierarchy(tree.walk(), point)
+
+    assert len(returned_user_symbols.keys()) == len(symbols)
+    assert len(returned_keywords.keys()) == len(keywords)
+
+    # check hierarchy levels are correct
+    for symbol, lvl in symbols:
+        assert symbol in returned_user_symbols
+        assert returned_user_symbols[symbol][1] == lvl
+    for keyword, lvl in keywords:
+        assert keyword in returned_keywords
+        assert returned_keywords[keyword][1] == lvl
+
+def test_find_symbols_in_asset2_hierarchy(mal_find_symbols_in_scope):
+    tree = PARSER.parse(mal_find_symbols_in_scope.read())
+
+    # space between category and asset (category scope)
+    point = (12, 4)
+
+    # symbols (identifiers + keywords)
+    symbols = [
+        ("destroy",0),
+        ('Asset1',1),
+        ('Asset2',1),
+        ('Asset3',1),
+    ]
+    keywords = [ 
+        ("extends",1),
+        ("abstract",1),
+        ("asset",1),
+        ("category",2),
+        ("associations",2),
+    ]
+
+    # we use sets to ensure order does not matter
+    returned_user_symbols, returned_keywords = find_symbols_in_context_hierarchy(tree.walk(), point)
+
+    assert len(returned_user_symbols.keys()) == len(symbols)
+    assert len(returned_keywords.keys()) == len(keywords)
+
+    # check hierarchy levels are correct
+    for symbol, lvl in symbols:
+        assert symbol in returned_user_symbols
+        assert returned_user_symbols[symbol][1] == lvl
+    for keyword, lvl in keywords:
+        assert keyword in returned_keywords
+        assert returned_keywords[keyword][1] == lvl
+
