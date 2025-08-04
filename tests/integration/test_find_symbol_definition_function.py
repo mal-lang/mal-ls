09883b0b36db4b219ad780c623399ecb9fdb6a32
Tomas Fonseca <tomas.s.fonseca@tecnico.ulisboa.pt>
2025-08-04 17:58:26 +0200

    tests: Find symbol definition for category.

---
+++ tests/integration/test_find_symbol_definition_function.py
@@ -0,0 +1,27 @@
+import tree_sitter_mal as ts_mal
+from tree_sitter import Language, Parser
+
+from malls.ts.utils import find_symbol_definition
+
+MAL_LANGUAGE = Language(ts_mal.language())
+PARSER = Parser(MAL_LANGUAGE)
+
+
+def test_symbol_definition_in_category_declaration(mal_find_symbols_in_scope):
+    tree = PARSER.parse(mal_find_symbols_in_scope.read())
+
+    # go to name of category
+    point = (3, 12)
+
+    # get the node
+    cursor = tree.walk()
+    while (cursor.goto_first_child_for_point(point) != None): continue
+
+    # confirm it's an identifier
+    assert cursor.node.type == 'identifier'
+
+    # we use sets to ensure order does not matter
+    response = find_symbol_definition(cursor.node, cursor.node.text)
+
+    # ensure position is start of category declaration
+    assert response == (3,0)
