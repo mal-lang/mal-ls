b368da96de722e7b2ce67d4ca6b491bba9d393e7
Tomas Fonseca <tomas.s.fonseca@tecnico.ulisboa.pt>
2025-08-05 18:23:37 +0200

    tests: Find symbol for extended asset.

--- tests/integration/test_find_symbol_definition_function.py
+++ tests/integration/test_find_symbol_definition_function.py
@@ -1,10 +1,16 @@
 import tree_sitter_mal as ts_mal
+import logging
 from tree_sitter import Language, Parser
+from pathlib import Path
 
-from malls.ts.utils import find_symbol_definition
+from malls.ts.utils import find_symbol_definition, INCLUDED_FILES_QUERY, run_query
+from malls.lsp.utils import recursive_parsing
+from malls.lsp.classes import Document
 
+log = logging.getLogger(__name__)
 MAL_LANGUAGE = Language(ts_mal.language())
 PARSER = Parser(MAL_LANGUAGE)
+FILE_PATH = str(Path(__file__).parent.parent.resolve()) + "/fixtures/mal/"
 
 
 def test_symbol_definition_in_category_declaration(mal_find_symbols_in_scope):
@@ -15,30 +21,32 @@ def test_symbol_definition_in_category_declaration(mal_find_symbols_in_scope):
 
     # get the node
     cursor = tree.walk()
-    while (cursor.goto_first_child_for_point(point) != None): continue
+    while cursor.goto_first_child_for_point(point) is not None:
+        continue
 
     # confirm it's an identifier
-    assert cursor.node.type == 'identifier'
+    assert cursor.node.type == "identifier"
 
     # we use sets to ensure order does not matter
     response = find_symbol_definition(cursor.node, cursor.node.text)
 
     # ensure position is start of category declaration
-    assert response == (3,0)
+    assert response == (3, 0)
 
 
-def test_symbol_definition_in_asset_declaration(mal_find_symbols_in_scope):
+def test_symbol_definition_in_asset_declaration_asset_name(mal_find_symbols_in_scope):
     tree = PARSER.parse(mal_find_symbols_in_scope.read())
 
     # go to name of asset
-    point = (5, 19)
+    point = (11, 10)
 
     # get the node
     cursor = tree.walk()
-    while (cursor.goto_first_child_for_point(point) != None): continue
+    while cursor.goto_first_child_for_point(point) is not None:
+        continue
 
     # confirm it's an identifier
-    assert cursor.node.type == 'identifier'
+    assert cursor.node.type == "identifier"
 
     # we use sets to ensure order does not matter
     response = find_symbol_definition(cursor.node, cursor.node.text)
@@ -46,4 +54,43 @@ def test_symbol_definition_in_asset_declaration(mal_find_symbols_in_scope):
     # ensure position is start of asset declaration
     # we have to make sure we ignore whitespaces,
     # the only thing that maters are non-empty characters
-    assert response == (5,4)
+    assert response == (11, 4)
+
+def test_symbol_definition_in_asset_declaration_extended_asset(
+    mal_symbol_def_extended_asset_main,
+):
+    # build the storage (mimicks the file parsing in the server)
+    storage = {}
+
+    doc_uri = FILE_PATH+"symbol_def_extended_asset_main.mal"
+    source_encoded = mal_symbol_def_extended_asset_main.read()
+    tree = PARSER.parse(source_encoded)
+
+    storage[doc_uri] = Document(tree, source_encoded, doc_uri)
+
+    # obtain the included files
+    root_node = tree.root_node
+
+    captures = run_query(root_node, INCLUDED_FILES_QUERY)
+    recursive_parsing(FILE_PATH, captures['file_name'], storage, doc_uri)
+
+    ###################################
+
+    # go to name of asset
+    point = (6, 25)
+
+    # get the node
+    cursor = tree.walk()
+    while cursor.goto_first_child_for_point(point) is not None:
+        continue
+
+    # confirm it's an identifier
+    assert cursor.node.type == "identifier"
+
+    # we use sets to ensure order does not matter
+    response = find_symbol_definition(cursor.node, cursor.node.text, doc_uri, storage)
+
+    # ensure position is start of asset declaration
+    # found in the 3rd auxiliary file
+    assert response == (2, 4)
+
