473adff46db593af495f78e9497676b1e228d3c5
Tomas Fonseca <tomas.s.fonseca@tecnico.ulisboa.pt>
2025-08-06 17:00:20 +0200

    tests: Symbol definition for asset variable.

--- tests/integration/test_find_symbol_definition_function.py
+++ tests/integration/test_find_symbol_definition_function.py
@@ -1,11 +1,12 @@
-import tree_sitter_mal as ts_mal
 import logging
-from tree_sitter import Language, Parser
 from pathlib import Path
 
-from malls.ts.utils import find_symbol_definition, INCLUDED_FILES_QUERY, run_query
-from malls.lsp.utils import recursive_parsing
+import tree_sitter_mal as ts_mal
+from tree_sitter import Language, Parser
+
 from malls.lsp.classes import Document
+from malls.lsp.utils import recursive_parsing
+from malls.ts.utils import INCLUDED_FILES_QUERY, find_symbol_definition, run_query
 
 log = logging.getLogger(__name__)
 MAL_LANGUAGE = Language(ts_mal.language())
@@ -27,7 +28,6 @@ def test_symbol_definition_in_category_declaration(mal_find_symbols_in_scope):
     # confirm it's an identifier
     assert cursor.node.type == "identifier"
 
-    # we use sets to ensure order does not matter
     response = find_symbol_definition(cursor.node, cursor.node.text)
 
     # ensure position is start of category declaration
@@ -48,7 +48,6 @@ def test_symbol_definition_in_asset_declaration_asset_name(mal_find_symbols_in_s
     # confirm it's an identifier
     assert cursor.node.type == "identifier"
 
-    # we use sets to ensure order does not matter
     response = find_symbol_definition(cursor.node, cursor.node.text)
 
     # ensure position is start of asset declaration
@@ -56,13 +55,14 @@ def test_symbol_definition_in_asset_declaration_asset_name(mal_find_symbols_in_s
     # the only thing that maters are non-empty characters
     assert response == (11, 4)
 
+
 def test_symbol_definition_in_asset_declaration_extended_asset(
     mal_symbol_def_extended_asset_main,
 ):
     # build the storage (mimicks the file parsing in the server)
     storage = {}
 
-    doc_uri = FILE_PATH+"symbol_def_extended_asset_main.mal"
+    doc_uri = FILE_PATH + "symbol_def_extended_asset_main.mal"
     source_encoded = mal_symbol_def_extended_asset_main.read()
     tree = PARSER.parse(source_encoded)
 
@@ -72,7 +72,7 @@ def test_symbol_definition_in_asset_declaration_extended_asset(
     root_node = tree.root_node
 
     captures = run_query(root_node, INCLUDED_FILES_QUERY)
-    recursive_parsing(FILE_PATH, captures['file_name'], storage, doc_uri)
+    recursive_parsing(FILE_PATH, captures["file_name"], storage, doc_uri)
 
     ###################################
 
@@ -94,3 +94,24 @@ def test_symbol_definition_in_asset_declaration_extended_asset(
     # found in the 3rd auxiliary file
     assert response == (2, 4)
 
+
+def test_symbol_definition_in_asset_variable(mal_find_symbols_in_scope):
+    tree = PARSER.parse(mal_find_symbols_in_scope.read())
+
+    # go to name of variable
+    point = (7, 10)
+
+    # get the node
+    cursor = tree.walk()
+    while cursor.goto_first_child_for_point(point) is not None:
+        continue
+
+    # confirm it's an identifier
+    assert cursor.node.type == "identifier"
+
+    response = find_symbol_definition(cursor.node, cursor.node.text)
+
+    # ensure position is start of asset declaration
+    # we have to make sure we ignore whitespaces,
+    # the only thing that maters are non-empty characters
+    assert response == (7, 6)
