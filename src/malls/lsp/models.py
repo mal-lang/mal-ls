475bbda8eaa7e2aadfcdb7d63b810ef30aa0a60f
Tomas Fonseca <tomas.s.fonseca@tecnico.ulisboa.pt>
2025-08-07 17:01:51 +0200

    fix: Corrected linter errors.

--- src/malls/lsp/models.py
+++ src/malls/lsp/models.py
@@ -2616,6 +2616,7 @@ class DidOpenTextDocumentParams(BaseModel):
 
     textDocument: TextDocumentItem
 
+
 class WholeFileChange(BaseModel):
     """
     Class to represent a change to the whole file
@@ -2623,10 +2624,12 @@ class WholeFileChange(BaseModel):
 
     text: str
 
+
 class TextDocumentContentChangeEvent(BaseModel):
     """
     https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocumentContentChangeEvent
     """
+
     range: Range
 
     range_length: int | None = None
@@ -2635,13 +2638,14 @@ class TextDocumentContentChangeEvent(BaseModel):
 
     model_config = base_config
 
+
 class DidChangeTextDocumentParams(BaseModel):
     """
     https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#didChangeTextDocumentParams
     """
+
     text_document: VersionedTextDocumentIdentifier
 
     content_changes: list[TextDocumentContentChangeEvent]
 
     model_config = base_config
-
