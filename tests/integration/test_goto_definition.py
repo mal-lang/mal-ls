b47b6d1f61f26ec22bdca8448a109ac686c385e2
Tomás Fonseca <tfonseca@kth.se>
2025-08-13 10:26:08 +0100

    tests: Go To definition with LSP.

--- tests/integration/test_goto_definition.py
+++ tests/integration/test_goto_definition.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+import pytest
 import typing
 
 import tree_sitter_mal as ts_mal
@@ -28,12 +29,43 @@ def filepath_to_uri(filepath: str) -> str:
 
     return absolute_path_obj.as_uri()
 
-
-def test_goto_definition_category_declaration(
-    find_symbol_1: typing.BinaryIO,
+find_symbols_in_scope_expected_results = [
+    ("goto_def_1", (3,0), "find_symbols_in_scope"),
+    ("goto_def_2", (11,4), "find_symbols_in_scope"),
+    ("goto_def_3", (7,6), "find_symbols_in_scope"),
+    ("goto_def_4", (8,8), "find_symbols_in_scope"),
+    ("goto_def_5", (2,4), "symbol_def_extended_asset_aux3"),
+    ("goto_def_6", (4,6), "symbol_def_extended_asset_aux3"),
+    ("goto_def_7", (5,6), "symbol_def_variable_call_extend_chain_aux2"),
+    ("goto_def_8", (5,4), "symbol_def_variable_declaration_main"),
+    ("goto_def_9", (13,4), "symbol_def_variable_declaration_main"),
+    ("goto_def_10", (15,4), "symbol_def_variable_declaration_main"),
+    ("goto_def_11", (15,4), "symbol_def_variable_declaration_main"),
+    ("goto_def_12", (8,4), "symbol_def_variable_declaration_main"),
+    ("goto_def_13", (7,4), "symbol_def_variable_declaration_main"),
+    ("goto_def_14", (28,4), "symbol_def_variable_declaration_main"),
+    ("goto_def_15", (28,4), "symbol_def_variable_declaration_main"),
+    ("goto_def_16", (30,4), "symbol_def_variable_declaration_main"),
+    ("goto_def_17", (5,4), "symbol_def_preconditions"),
+    ("goto_def_18", (14,4), "symbol_def_preconditions"),
+    ("goto_def_19", (16,4), "symbol_def_preconditions"),
+    ("goto_def_20", (16,4), "symbol_def_preconditions"),
+    ("goto_def_21", (6,8), "symbol_def_reaches"),
+    ("goto_def_22", (15,6), "symbol_def_reaches"),
+]
+@pytest.mark.parametrize(
+    "fixture_name,expected_point,expected_file",
+    [
+        (fixture_name, point, file)
+        for fixture_name, point, file in find_symbols_in_scope_expected_results
+    ],
+)
+def test_goto_definition(
+    request, fixture_name, expected_point, expected_file
 ):
     # send to server
-    output, ls, *_ = server_output(find_symbol_1)
+    fixture = request.getfixturevalue(fixture_name)
+    output, ls, *_ = server_output(fixture)
 
     output.seek(0)
     response = get_lsp_json(output)
@@ -44,7 +76,7 @@ def test_goto_definition_category_declaration(
     start_point = (start_point_dict["line"], start_point_dict["character"])
     file = response["result"]["uri"]
 
-    assert start_point == (3,0)
-    assert file == filepath_to_uri(FILE_PATH+"find_symbols_in_scope.mal")
+    assert start_point == expected_point
+    assert file == filepath_to_uri(FILE_PATH+expected_file+".mal")
 
     output.close()
