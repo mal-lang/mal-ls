574abc75d30314774131f8bfa7d146e514644e4d
Tomás Fonseca <tfonseca@kth.se>
2025-08-18 12:55:55 +0100

    tests: Find meta comment for asset variable.

--- tests/integration/test_find_meta_comment_function.py
+++ tests/integration/test_find_meta_comment_function.py
@@ -1,16 +1,21 @@
 import tree_sitter_mal as ts_mal
 import pytest
 from tree_sitter import Language, Parser
+from pathlib import Path
 
-from malls.ts.utils import find_meta_comment_function
+from malls.lsp.classes import Document
+from malls.lsp.utils import recursive_parsing
+from malls.ts.utils import INCLUDED_FILES_QUERY, find_meta_comment_function, run_query
 
 MAL_LANGUAGE = Language(ts_mal.language())
 PARSER = Parser(MAL_LANGUAGE)
+FILE_PATH = str(Path(__file__).parent.parent.resolve()) + "/fixtures/mal/"
 
 parameters = [
     ((3, 12), [b"dev cat", b"mod cat"]),
     ((8, 22), [b"dev asset", b"mod asset"]),
     ((13, 13), [b"dev attack_step", b"mod attack_step"]),
+    ((12, 18), [b"dev asset3", b"mod asset3"]),
 ]
 
 @pytest.mark.parametrize(
@@ -18,7 +23,23 @@ parameters = [
     [(point, comment) for point, comment in parameters],
 )
 def test_find_meta_comment_function(mal_find_meta_comment_function, point, comments):
-    tree = PARSER.parse(mal_find_meta_comment_function.read())
+    # build the storage (mimicks the file parsing in the server)
+    storage = {}
+
+    doc_uri = FILE_PATH + "find_meta_comment_function.mal"
+    source_encoded = mal_find_meta_comment_function.read()
+    tree = PARSER.parse(source_encoded)
+
+    storage[doc_uri] = Document(tree, source_encoded, doc_uri)
+
+    # obtain the included files
+    root_node = tree.root_node
+
+    captures = run_query(root_node, INCLUDED_FILES_QUERY)
+    if "file_name" in captures:
+        recursive_parsing(FILE_PATH, captures["file_name"], storage, doc_uri, [])
+
+    ###################################
 
     # get the node
     cursor = tree.walk()
@@ -29,6 +50,6 @@ def test_find_meta_comment_function(mal_find_meta_comment_function, point, comme
     assert cursor.node.type == "identifier"
 
     # we use sets to ensure order does not matter
-    returned_comments = find_meta_comment_function(cursor.node, cursor.node.text)
+    returned_comments = find_meta_comment_function(cursor.node, cursor.node.text, doc_uri, storage)
 
     assert set(returned_comments) == set(comments)
